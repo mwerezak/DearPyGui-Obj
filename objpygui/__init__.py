60b1b3811dfaed9c9c69c7bdfd9d685bbc58948a
mwerezak <mwerezak@gmail.com>
2021-02-24 01:19:27 -0500

    Remove package-level imports

--- objpygui/__init__.py
+++ objpygui/__init__.py
@@ -1,10 +1,1 @@
 """Object-Oriented Wrapper around DearPyGui 0.6"""
-
-from objpygui.core import (
-    get_item_by_id,
-    iter_all_items,
-    GuiItem,
-)
-
-from objpygui.button import Button
-from objpygui.window import Window
