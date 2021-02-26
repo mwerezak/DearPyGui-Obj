cc23a6e1b68d1dec8b82a0767e2c10bd97ca3526
Mike Werezak <mwerezak@gmail.com>
2021-02-26 17:58:53 -0500

    Add SliderFloat widgets

--- dearpygui_obj/input.py
+++ dearpygui_obj/input.py
@@ -8,6 +8,7 @@ from dearpygui_obj.wrapper import PyGuiBase, dearpygui_wrapper, ConfigProperty
 if TYPE_CHECKING:
     from typing import Optional
 
+## Input Boxes
 
 @dearpygui_wrapper('mvAppItemType::InputText')
 class InputText(PyGuiBase):
@@ -143,6 +144,60 @@ class InputInt4(InputInt):
         dpgcore.add_input_int4(self.id, **config)
 
 
+## Sliders
+
+@dearpygui_wrapper('mvAppItemType::SliderFloat')
+class SliderFloat(PyGuiBase):
+    """A slider for a float value.
+
+    If not disabled using the :attr:`no_input` property, the slider can be CTRL+Clicked to turn it
+    into an input box for manual input of a value."""
+
+    label: str = ConfigProperty()
+    min_value: float = ConfigProperty()
+    max_value: float = ConfigProperty()
+    format: str = ConfigProperty()
+    vertical: bool = ConfigProperty()
+
+    #: Control whether a value can be manually entered using CTRL+Click
+    no_input: bool = ConfigProperty()
+
+    #: Whether to clamp the value when using manual input. By default CTRL+Click allows going out of bounds.
+    clamped: bool = ConfigProperty()
+
+    def _setup_add_widget(self, config) -> None:
+        dpgcore.add_slider_float(self.id, **config)
+
+@dearpygui_wrapper('mvAppItemType::SliderFloat2')
+class SliderFloat2(SliderFloat):
+    """A slider for 2 float values.
+
+    If not disabled using the :attr:`no_input` property, the slider can be CTRL+Clicked to turn it
+    into an input box for manual input of a value."""
+
+    def _setup_add_widget(self, config) -> None:
+        dpgcore.add_slider_float2(self.id, **config)
+
+@dearpygui_wrapper('mvAppItemType::SliderFloat3')
+class SliderFloat3(SliderFloat):
+    """A slider for 3 float values.
+
+    If not disabled using the :attr:`no_input` property, the slider can be CTRL+Clicked to turn it
+    into an input box for manual input of a value."""
+
+    def _setup_add_widget(self, config) -> None:
+        dpgcore.add_slider_float3(self.id, **config)
+
+@dearpygui_wrapper('mvAppItemType::SliderFloat4')
+class SliderFloat4(SliderFloat):
+    """A slider for 4 float values.
+
+    If not disabled using the :attr:`no_input` property, the slider can be CTRL+Clicked to turn it
+    into an input box for manual input of a value."""
+
+    def _setup_add_widget(self, config) -> None:
+        dpgcore.add_slider_float4(self.id, **config)
+
 if __name__ == '__main__':
     from dearpygui.core import *
     from dearpygui_obj.window import Window
@@ -153,15 +208,14 @@ if __name__ == '__main__':
         t = InputText('InputText')
         t2 = InputText('Deleted')
         f = InputFloat('InputFloat', default_value=4.3)
-        f2 = InputFloat2('InputFloat2', tip='tooltip')
+        f2 = InputFloat2('InputFloat2', tooltip='tooltip')
         i = InputInt('InputInt', data_source = linked_ints)
         i2 = InputInt2('InputInt2', data_source = linked_ints)
         i4 = InputInt4('InputInt4', data_source = linked_ints)
+        sf = SliderFloat('SliderFloat')
+        sf3 = SliderFloat3('SliderFloat3')
+        print(get_item_type(sf.id))
+        print(get_item_configuration(sf.id))
 
-    get_item_type(f2.id)
-    print(f2.tip)
-
-    t2.delete()
-    print(t2.is_valid)
 
     start_dearpygui()
