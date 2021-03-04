a7aeb48354c6202aaba507fd430399531ccfeb3d
Mike Werezak <mwerezak@gmail.com>
2021-03-03 21:20:06 -0500

    Add devtool classmethods to access standard tool instances

--- dearpygui_obj/devtools.py
+++ dearpygui_obj/devtools.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import dearpygui.core as dpgcore
-from dearpygui_obj import _register_item_type
+from dearpygui_obj import _register_item_type, get_item_by_id
 from dearpygui_obj.window import Window
 
 if TYPE_CHECKING:
@@ -16,12 +16,31 @@ class DebugWindow(Window):
         dpgcore.add_debug_window(self.id, **dpg_args)
         dpgcore.end()
 
+    @classmethod
+    def get_instance(cls):
+        """Get the standard instance that is automatically created by DPG."""
+        return get_item_by_id('debug##standard')
+
+    @classmethod
+    def show_debug(cls) -> None:
+        cls.get_instance().show = True
+
+
 @_register_item_type('mvAppItemType::MetricsWindow')
 class MetricsWindow(Window):
     """Developer tool, creates a metrics window."""
     def _setup_add_widget(self, dpg_args) -> None:
         dpgcore.add_metrics_window(self.id, **dpg_args)
         dpgcore.end()
+
+    @classmethod
+    def get_instance(cls):
+        """Get the standard instance that is automatically created by DPG."""
+        return get_item_by_id('metrics##standard')
+
+    @classmethod
+    def show_metrics(cls) -> None:
+        cls.get_instance().show = True
 
 @_register_item_type('mvAppItemType::StyleWindow')
 class StyleEditorWindow(Window):
@@ -30,6 +49,15 @@ class StyleEditorWindow(Window):
         dpgcore.add_style_window(self.id, **dpg_args)
         dpgcore.end()
 
+    @classmethod
+    def get_instance(cls):
+        """Get the standard instance that is automatically created by DPG."""
+        return get_item_by_id('style##standard')
+
+    @classmethod
+    def show_style_editor(cls) -> None:
+        cls.get_instance().show = True
+
 @_register_item_type('mvAppItemType::DocWindow')
 class DocumentationWindow(Window):
     """Developer tool, creates a window showing DearPyGui documentation."""
@@ -37,9 +65,27 @@ class DocumentationWindow(Window):
         dpgcore.add_doc_window(self.id, **dpg_args)
         dpgcore.end()
 
+    @classmethod
+    def get_instance(cls):
+        """Get the standard instance that is automatically created by DPG."""
+        return get_item_by_id('documentation##standard')
+
+    @classmethod
+    def show_documentation(cls) -> None:
+        cls.get_instance().show = True
+
 @_register_item_type('mvAppItemType::AboutWindow')
 class AboutWindow(Window):
     """Developer tool, creates window containing information about DearPyGui."""
     def _setup_add_widget(self, dpg_args) -> None:
         dpgcore.add_about_window(self.id, **dpg_args)
         dpgcore.end()
+
+    @classmethod
+    def get_instance(cls):
+        """Get the standard instance that is automatically created by DPG."""
+        return get_item_by_id('about##standard')
+
+    @classmethod
+    def show_about(cls) -> None:
+        cls.get_instance().show = True
